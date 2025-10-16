695592a14096622d3d39b2c44f1a72ecef32834f
dhruvpatel <pate5491@shernet.sheridancollege.ca>
2025-10-16 12:40:28 -0400

    Added code in Classes

--- MidtermAppDhruv/Models/PhoneOrder.cs
+++ MidtermAppDhruv/Models/PhoneOrder.cs
@@ -2,5 +2,8 @@ namespace MidtermAppDhruv.Models;
 
 public class PhoneOrder
 {
-    
+    public string CustomerName {get; set;}
+    public string PhoneType {get; set;}
+    public string PhoneColor {get; set;}
+    public int Quantity {get; set;}
 }
--- MidtermAppDhruv/Models/Receipt.cs
+++ MidtermAppDhruv/Models/Receipt.cs
@@ -2,5 +2,13 @@ namespace MidtermAppDhruv.Models;
 
 public class Receipt
 {
-    
+    public string CustomerName { get; set; }
+    public string PhoneType { get; set; }
+    public string PhoneColor { get; set; }
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+    public decimal SubTotal { get; set; }
+    public decimal Discount { get; set; }
+    public decimal Tax { get; set; }
+    public decimal Total { get; set; }
 }
